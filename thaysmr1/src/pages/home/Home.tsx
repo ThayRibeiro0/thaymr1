5f2997465900d53236b2dcaf5ed2e4aec25662ad
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-11-16 00:09:53 -0800

    9

--- thaysmr1/src/pages/home/Home.tsx
+++ thaysmr1/src/pages/home/Home.tsx
@@ -1,15 +1,27 @@
-import background from '../../assets/backvideo.mp4'
-import { Link } from 'react-router-dom'
-import { FaArrowRight } from 'react-icons/fa'
+import './home.css'
+import { motion } from 'framer-motion'
+import background from '../../assets/backvideo.mp4';
+import Navbar from '../../components/Navbar'; // Import your Navbar component
 
 const Home = () => {
   return (
-    <section className='home section grid'>
-        <video autoPlay loop muted id='bg-video'> 
-          <source src={background} type="video/mp4" />
+    <section className='home-container'>
+      <div className='video-container'>
+        <video autoPlay loop muted id='bg-video'>
+          <source src={background} type='video/mp4' />
         </video>
-    </section>
-  )
-}
+      </div>
+      <Navbar /> 
 
-export default Home
+      <div className='content-text'>
+          <h1>Hi, I am <span className='myname'>&nbsp;Thays Moia Ribeiro</span></h1>
+          <p>I have Graduation degree in Law ⚖, and Association degree in System Developement 💻and Administration 📊</p>
+        </div>
+        <div className='content-people' style={{width: 1500, height: 400, marginTop: -100}}>
+          {/* <ComputersCanvas /> */}
+        </div>
+    </section>
+  );
+};
+
+export default Home;
