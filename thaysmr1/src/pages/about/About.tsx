4ee95f8ad6334e107c727f3c0efde8fdb0fefc23
Thays Ribeiro <94051605+ThayRibeiro0@users.noreply.github.com>
2023-11-26 20:53:39 -0800

    2

--- thaysmr1/src/pages/about/About.tsx
+++ thaysmr1/src/pages/about/About.tsx
@@ -1,5 +1,6 @@
 import '../about/about.css'
 import Me from '../../assets/mypic.jpg'
+import { FcReading } from "react-icons/fc";
 
 const About = () => {
   return (
@@ -9,7 +10,7 @@ const About = () => {
       </div>
       <div className='subBoxText'>
         <p>Introduction</p>
-        <h2>Overview</h2>   
+        <h2>Overview <FcReading /></h2>   
         <p>I am a skilled software developer with experience in in so many areas (as sales, law, administrative, cleaner, banker, aupair) and with code in HTML, CSS, Javascript, Python, React, Three.js, Node, Express, PHP, C, JAVA, Raspberry PI and and Aplications, Handlebars, SQL, Android Studio, GitHub, Visual Studio Code, Codespaces, Web Systems Project, Programming Logic, Portugol and others, while also being an aupair. "Continue learning by myself."(Unknown author)</p>
       </div>
     </section>
